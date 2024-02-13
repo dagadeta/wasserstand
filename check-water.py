616094841ddaf30f548280e6ba62d35490fa4b63
dagadeta <treriwin@gmail.com>
2024-02-13 11:20:58 +0100

    added check-water.py

---
+++ check-water.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+import os
+
+GPIO.setmode(GPIO.BCM)
+clear = lambda: os.system("clear")
+
+sensor_5 = 5
+GPIO.setup(sensor_5, GPIO.IN)
+sensor_4 = 6
+GPIO.setup(sensor_4, GPIO.IN)
+sensor_3 = 13
+GPIO.setup(sensor_3, GPIO.IN)
+sensor_2 = 19
+GPIO.setup(sensor_2, GPIO.IN)
+sensor_1 = 26
+GPIO.setup(sensor_1, GPIO.IN)
+
+try:
+    while True:
+        clear()
+        if GPIO.input(sensor_5) == GPIO.LOW:
+            print("sensor_5: Strom fließt")
+        else:
+            print("sensor_5: Kein Strom")
+        if GPIO.input(sensor_4) == GPIO.LOW:
+            print("sensor_4: Strom fließt")
+        else:
+            print("sensor_4: Kein Strom")
+        if GPIO.input(sensor_3) == GPIO.LOW:
+            print("sensor_3: Strom fließt")
+        else:
+            print("sensor_3: Kein Strom")
+        if GPIO.input(sensor_2) == GPIO.LOW:
+            print("sensor_2: Strom fließt")
+        else:
+            print("sensor_2: Kein Strom")
+        if GPIO.input(sensor_1) == GPIO.LOW:
+            print("sensor_1: Strom fließt")
+        else:
+            print("sensor_1: Kein Strom")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
